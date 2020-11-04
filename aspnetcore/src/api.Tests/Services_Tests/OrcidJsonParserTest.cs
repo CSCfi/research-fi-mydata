53522f19dff0d9cec0e9ffe2fc74f89ee36f28ac
Timo Särkikoski <sarkikos@gmail.com>
2020-11-04 15:07:43 +0200

    Added files which were accidentally left out.

---
+++ aspnetcore/src/api.Tests/Services_Tests/OrcidJsonParserTest.cs
@@ -0,0 +1,22 @@
+using System;
+using Xunit;
+using api.Services;
+using System.IO;
+
+namespace api.Tests
+{
+    [Collection("Parsing data from ORCID record json")]
+    public class OrcidJsonParserTests
+    {
+        [Fact(DisplayName = "Get biography")]
+        public void TestGetBiography()
+        {
+            var orcidJsonParserService = new OrcidJsonParserService();
+            var expectedBiography = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+            var filePath = $@"{Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName}/Infrastructure/testDataOrcidRecord.json";
+            var reader = new StreamReader(filePath);
+            var jsonStr = reader.ReadToEnd();
+            Assert.Equal(expectedBiography, orcidJsonParserService.GetBiography(jsonStr));
+        }
+    }
+}
