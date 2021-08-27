a543acb7f6bec87ccbc6c97c69ecaae24e94c3ed
Timo Särkikoski <sarkikos@gmail.com>
2021-08-27 10:28:07 +0300

    Add service for handling Elasticsearch update

---
+++ aspnetcore/src/api/Services/ElasticsearchService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace api.Services
+{
+    /*
+     * ElasticsearchService handles person index update.
+     */
+    public class ElasticsearchService
+    {
+        public HttpClient Client { get; }
+        private readonly ILogger<ElasticsearchService> _logger;
+
+        public ElasticsearchService(HttpClient client, ILogger<ElasticsearchService> logger)
+        {
+            //client.BaseAddress = new Uri("https://<api address>");
+            Client = client;
+            _logger = logger;
+        }
+    }
+}
