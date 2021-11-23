c939e92a276133548e669104b19d723b648ca44e
Timo Särkikoski <sarkikos@gmail.com>
2021-11-23 18:12:50 +0200

    Add controller and models for DimFundingDecision handling.

---
+++ aspnetcore/src/api/Models/Api/ApiResponseFundingPostMany.cs
@@ -0,0 +1,18 @@
+﻿using api.Models.ProfileEditor;
+
+namespace api.Models
+{
+    public class ApiResponseFundingPostMany : ApiResponse
+    {
+
+        public ApiResponseFundingPostMany(bool success, string reason, ProfileEditorAddFundingResponse data, bool fromCache)
+        {
+            Success = true;
+            Reason = null;
+            Data = data;
+            FromCache = false;
+        }
+
+        public ProfileEditorAddFundingResponse Data { get; set; }
+    }
+}
---
+++ aspnetcore/src/api/Models/ProfileEditor/ProfileEditorItemFunding.cs
@@ -0,0 +1,12 @@
+﻿namespace api.Models.ProfileEditor
+{
+    public partial class ProfileEditorItemFunding : ProfileEditorItem
+    {
+        public ProfileEditorItemFunding()
+        {
+        }
+
+        // TODO: modify according to model
+        public int Id { get; set; }
+    }
+}
