e0d92fc80749269792731e4ed9727c9ce681df94
Timo Särkikoski <sarkikos@gmail.com>
2022-05-13 10:02:30 +0300

    Add missing model files.

---
+++ aspnetcore/src/api/Models/Ttv/BrDatasetDatasetRelationship.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace api.Models.Ttv
+{
+    public partial class BrDatasetDatasetRelationship
+    {
+        public int DimResearchDatasetId { get; set; }
+        public int DimResearchDatasetId2 { get; set; }
+        public string Type { get; set; }
+
+        public virtual DimResearchDataset DimResearchDataset { get; set; }
+        public virtual DimResearchDataset DimResearchDatasetId2Navigation { get; set; }
+    }
+}
