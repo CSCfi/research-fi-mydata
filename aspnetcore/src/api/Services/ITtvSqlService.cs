4e4d43b286ab6be48e2a0e084b0571fb7cf84df5
Timo Särkikoski <sarkikos@gmail.com>
2022-08-24 15:46:30 +0300

    Add missing ITtvSqlService.cs, which was dropped by malfunctioning IDE.

---
+++ aspnetcore/src/api/Services/ITtvSqlService.cs
@@ -0,0 +1,11 @@
+﻿using api.Models.ProfileEditor;
+
+namespace api.Services
+{
+    public interface ITtvSqlService
+    {
+        string GetFactFieldValuesFKColumnNameFromFieldIdentifier(int fieldIdentifier);
+        string GetSqlQuery_ProfileData(int userProfileId);
+        string GetSqlQuery_Update_FactFieldValues(int dimUserProfileId, ProfileEditorItemMeta profileEditorItemMeta);
+    }
+}
