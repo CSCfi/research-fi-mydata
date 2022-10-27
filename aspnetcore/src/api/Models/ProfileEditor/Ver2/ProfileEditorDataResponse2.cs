30a38ca78a26b4c3a7493a39794411fb39909011
Timo Särkikoski <sarkikos@gmail.com>
2022-10-27 10:15:11 +0300

    Add list of unique data sources in profile data and Elasticsearch index.

--- aspnetcore/src/api/Models/ProfileEditor/Ver2/ProfileEditorDataResponse2.cs
+++ aspnetcore/src/api/Models/ProfileEditor/Ver2/ProfileEditorDataResponse2.cs
@@ -1,13 +1,18 @@
-﻿namespace api.Models.ProfileEditor
+﻿using api.Models.Elasticsearch;
+using System.Collections.Generic;
+
+namespace api.Models.ProfileEditor
 {
     public partial class ProfileEditorDataResponse2    {
         public ProfileEditorDataResponse2()
         {
-            personal = new ProfileEditorDataPersonal2();
-            activity = new ProfileEditorDataActivity2();
+            personal = new();
+            activity = new();
+            uniqueDataSources = new();
         }
 
         public ProfileEditorDataPersonal2 personal { get; set; }
         public ProfileEditorDataActivity2 activity { get; set; }
+        public List<ProfileEditorSource> uniqueDataSources { get; set; }
     }
 }
