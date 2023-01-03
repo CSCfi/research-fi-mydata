8aaf5ef3f7e3856857a3e786d939235590d07225
Timo Särkikoski <sarkikos@gmail.com>
2023-01-03 15:10:58 +0200

    Ttv model update 3.1.2023 (#126)

---
+++ aspnetcore/src/api/Models/Ttv/TempBrFieldOfScienceDimFundingDecision.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace api.Models.Ttv
+{
+    public partial class TempBrFieldOfScienceDimFundingDecision
+    {
+        public int DimFieldOfScienceId { get; set; }
+        public int DimFundingDecisionId { get; set; }
+    }
+}
