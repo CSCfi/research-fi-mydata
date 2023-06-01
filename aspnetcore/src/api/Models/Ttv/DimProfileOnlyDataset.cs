c9481e6c2912f3b977708be188c5f0038b521118
Timo Särkikoski <sarkikos@gmail.com>
2023-06-01 10:26:54 +0300

    CSCTTV-3248 dim_web_link model change
    Add foreign keys to DimProfileOnlyDataset, DimProfileOnlyFundingDecision and DimProfileOnlyResearchActivity

--- aspnetcore/src/api/Models/Ttv/DimProfileOnlyDataset.cs
+++ aspnetcore/src/api/Models/Ttv/DimProfileOnlyDataset.cs
@@ -8,6 +8,7 @@ namespace api.Models.Ttv
         public DimProfileOnlyDataset()
         {
             DimPids = new HashSet<DimPid>();
+            DimWebLinks = new HashSet<DimWebLink>();
             FactFieldValues = new HashSet<FactFieldValue>();
         }
 
@@ -34,6 +35,7 @@ namespace api.Models.Ttv
         public virtual DimReferencedatum DimReferencedataIdAvailabilityNavigation { get; set; }
         public virtual DimRegisteredDataSource DimRegisteredDataSource { get; set; }
         public virtual ICollection<DimPid> DimPids { get; set; }
+        public virtual ICollection<DimWebLink> DimWebLinks { get; set; }
         public virtual ICollection<FactFieldValue> FactFieldValues { get; set; }
     }
 }
