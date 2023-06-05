d78d23ce8e1487d0048fc7cefc614ba1113758dd
Timo Särkikoski <sarkikos@gmail.com>
2023-06-05 14:28:47 +0300

    CSCTTV-3315 Add cooperation choices in Elasticsearch person index

---
+++ aspnetcore/src/api/Models/Elasticsearch/ElasticsearchCooperation.cs
@@ -0,0 +1,14 @@
+﻿namespace api.Models.Elasticsearch
+{
+    public partial class ElasticsearchCooperation
+    {
+        public ElasticsearchCooperation()
+        {
+        }
+
+        public int Id { get; set; }
+        public string NameFi { get; set; }
+        public string NameEn { get; set; }
+        public string NameSv { get; set; }
+    }
+}
