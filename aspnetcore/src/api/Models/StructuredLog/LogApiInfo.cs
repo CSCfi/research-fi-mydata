c4cbe4d8bc8a5b1b4263fb8aa04ae10d804a3480
Timo Särkikoski <sarkikos@gmail.com>
2023-08-18 14:57:15 +0300

    CSCTTV-2075 Add logging metadata for Wood logging service

--- aspnetcore/src/api/Models/StructuredLog/LogApiInfo.cs
+++ aspnetcore/src/api/Models/StructuredLog/LogApiInfo.cs
@@ -8,6 +8,7 @@
             State = state;
             Error = error;
             Message = message;
+            Wood = new LogWood();
         }
 
         public LogApiInfo(string action, string state, bool error)
@@ -16,6 +17,7 @@
             State = state;
             Error = error;
             Message = "";
+            Wood = new LogWood();
         }
 
         public LogApiInfo(string action, string state, string message)
@@ -24,6 +26,7 @@
             State = state;
             Error = false;
             Message = message;
+            Wood = new LogWood();
         }
 
         public LogApiInfo(string action, string state)
@@ -32,11 +35,13 @@
             State = state;
             Error = false;
             Message = "";
+            Wood = new LogWood();
         }
 
         public string Action { get; set; }
         public string State { get; set; }
         public bool Error { get; set; }
         public string Message { get; set; }
+        public LogWood Wood { get; set; }
     }
 }
