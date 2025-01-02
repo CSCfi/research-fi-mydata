469d877cd555a31a843586ff21e4eb9029dad83d
Timo Särkikoski <sarkikos@gmail.com>
2025-01-02 12:50:58 +0200

    CSCTTV-4139 DimName.Id changed from int to long. Catchup all other changes in the DB context.

---
+++ aspnetcore/src/api/Models/Ttv/FactKeyword.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace api.Models.Ttv;
+
+public partial class FactKeyword
+{
+    public int DimKeywordId { get; set; }
+
+    public int DimResearchProjectId { get; set; }
+
+    public string SourceId { get; set; }
+
+    public string SourceDescription { get; set; }
+
+    public DateTime? Created { get; set; }
+
+    public DateTime? Modified { get; set; }
+}
