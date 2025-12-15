2db5ef64ede6e1937d6a4262e4371c54bec9588e
Timo Särkikoski <sarkikos@gmail.com>
2025-12-15 11:35:06 +0200

    CSCTTV-4455 Add controller, models and services to manage AI generated researcher biography

---
+++ aspnetcore/src/api/Models/Aitta/AittaModel.cs
@@ -0,0 +1,116 @@
+using System;
+using System.Collections.Generic;
+
+namespace api.Models.Ai
+{
+    public partial class AittaModel
+    {
+        public AittaModel()
+        {
+        }
+
+        public List<AittaGrantedFunding> UserParticipatedGrantedFunding { get; set; } = new List<AittaGrantedFunding>();
+        public List<AittaResearchDataset> UserParticipatedDataset { get; set; } = new List<AittaResearchDataset>();
+        public List<string> ResearcherDescription { get; set; } = new List<string>();
+        public List<AittaEducation> HasCompleted { get; set; } = new List<AittaEducation>();
+        public List<AittaPublication> UserParticipatedPublication { get; set; } = new List<AittaPublication>();
+        public string PersonName { get; set; } = string.Empty;
+        public List<AittaResearchActivity> UserParticipatedActivity { get; set; } = new List<AittaResearchActivity>();
+        public List<AittaAffiliation> HasAffiliation { get; set; } = new List<AittaAffiliation>();
+    }
+
+    public class AittaKeyword
+    {
+        public string? KeywordContent { get; set; } = null;
+    }
+
+    /*
+    public class AittaDescriptiveItem
+    {
+        public string? DescriptiveContent { get; set; } = null;
+    }
+    */
+
+    public class AittaReferenceData
+    {
+        public string? CodeName { get; set; } = null;
+        public string? CodeValue { get; set; } = null;
+    }
+
+    public class AittaOrganization
+    {
+        public AittaOrganization? IsPartOfOrganization { get; set; } = null;
+        public string? OrganizationName { get; set; } = null;
+    }
+
+    public class AittaPublication
+    {
+        public List<string>? FieldsOfScience { get; set; } = null;
+        public int? Year { get; set; } = null;
+        public string Name { get; set; } = string.Empty;
+        public string? Type { get; set; } = null;
+        public string? TargetAudience { get; set; } = null;
+        public List<string>? Keywords { get; set; } = null;
+        // public string? Abstract { get; set; } = null;
+    }
+
+    public class AittaResearchActivity
+    {
+        public string? Description { get; set; } = null;
+        public string? Role { get; set; } = null;
+        public AittaDate? EndsOn { get; set; } = null;
+        public string? Type { get; set; } = null;
+        public AittaDate? StartsOn { get; set; } = null;
+        public string? ActivityTitle { get; set; } = null;
+    }
+    public class AittaResearchDataset
+    {
+        public string? Description { get; set; } = null;
+        public List<string>? FieldsOfScience { get; set; } = null;
+        public List<string>? Theme { get; set; } = null;
+        public List<string>? Keywords { get; set; } = null;
+        public string? DatasetTitle { get; set; } = null;
+        public DateTime? DatasetCreationDate { get; set; } = null;
+    }
+
+    public class AittaGrantedFunding
+    {
+        public List<string>? Theme { get; set; } = null;
+        public AittaDate? EndsOn { get; set; } = null;
+        public List<string>? FieldsOfScience { get; set; } = null;
+        public string? Description { get; set; } = null;
+        public string? TypeOfFunding { get; set; } = null;
+        public List<string>? Keywords { get; set; } = null;
+        public List<string>? FieldsOfResearch { get; set; } = null;
+        public AittaDate? StartsOn { get; set; } = null;
+        public AittaOrganization? HasFunder { get; set; } = null;
+        public string? Name { get; set; } = null;
+    }
+
+    public class AittaEducation_DegreeGrantingInstitution
+    {
+        public string? Description { get; set; } = null;
+        public string? Title { get; set; } = null;
+    }
+
+    public class AittaEducation
+    {
+        public string? DegreeGrantingInstitution { get; set; } = null;
+        public string? EducationName { get; set; } = null;
+    }
+
+    public class AittaAffiliation
+    {
+        public string? AffiliationType { get; set; } = null;
+        public string? PositionTitle { get; set; } = null;
+        public AittaOrganization? Organization { get; set; } = null;
+        public AittaDate? StartsOn { get; set; } = null;
+        public AittaDate? EndsOn { get; set; } = null;
+    }
+
+    public class AittaDate
+    {
+        public int? Year { get; set; }
+        public int? Month { get; set; }
+    }
+}
